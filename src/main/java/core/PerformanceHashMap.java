b4b72d75d21429371fc1d83b43a80b20647e7ec7
TonyYuta <tonykagaya@gmail.com>
2016-02-13 22:10:33 -0800

    Performance HashMap, Hashtable, LinkedHashMap, and TreeMap

--- src/main/java/core/PerformanceHashMap.java
+++ src/main/java/core/PerformanceHashMap.java
@@ -9,6 +9,9 @@
 package core;
 
 import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
 
 public class PerformanceHashMap {
 	
@@ -19,7 +22,9 @@ public class PerformanceHashMap {
 		long endTime;
 		long duration;
 		
+		// HashMap
 		HashMap<Integer, String> hm = new HashMap<Integer, String>();
+		
 		// PUT
 		startTime = System.nanoTime();
 		for (Integer i = 0; i < n; i++) {
@@ -28,6 +33,7 @@ public class PerformanceHashMap {
 		endTime = System.nanoTime();
 		duration = endTime - startTime;
 		System.out.println("HashMap puts:  " + duration);
+		
 		// GET
 		startTime = System.nanoTime();
 		for (int i = 0; i < n; i++) {
@@ -36,6 +42,7 @@ public class PerformanceHashMap {
 		endTime = System.nanoTime();
 		duration = endTime - startTime;
 		System.out.println("HashMap gets:  " + duration);
+		
 		// REMOVE
 		startTime = System.nanoTime();
 		for (int i = 0; i < n; i++) {
@@ -44,5 +51,97 @@ public class PerformanceHashMap {
 		endTime = System.nanoTime();
 		duration = endTime - startTime;
 		System.out.println("HashMap remove:  " + duration);
+		
+		// Hashtable
+		Hashtable<Integer, String> ht = new Hashtable<Integer, String>();
+		
+		// PUT
+		startTime = System.nanoTime();
+		for (Integer i = 0; i < n; i++) {
+			ht.put(i, "John Smith");
+		}
+		endTime = System.nanoTime();
+		duration = endTime - startTime;
+		System.out.println("Hashtable puts:  " + duration);
+		
+		// GET
+		startTime = System.nanoTime();
+		for (int i = 0; i < n; i++) {
+			ht.get(i);
+		}
+		endTime = System.nanoTime();
+		duration = endTime - startTime;
+		System.out.println("Hashtable gets:  " + duration);
+		
+		// REMOVE
+		startTime = System.nanoTime();
+		for (int i = 0; i < n; i++) {
+			ht.remove(i);
+		}
+		endTime = System.nanoTime();
+		duration = endTime - startTime;
+		System.out.println("Hashtable remove:  " + duration);
+		
+		// LinkedHashMap
+		LinkedHashMap lhm = new LinkedHashMap(); 
+			
+			// PUT
+			startTime = System.nanoTime();
+			for (Integer i = 0; i < n; i++) {
+				lhm.put(i, "John Smith");
+			}
+			endTime = System.nanoTime();
+			duration = endTime - startTime;
+			System.out.println("LinkedHashMap puts:  " + duration);
+			
+			// GET
+			startTime = System.nanoTime();
+			for (int i = 0; i < n; i++) {
+				lhm.get(i);
+			}
+			endTime = System.nanoTime();
+			duration = endTime - startTime;
+			System.out.println("LinkedHashMap gets:  " + duration);
+			
+			// REMOVE
+			startTime = System.nanoTime();
+			for (int i = 0; i < n; i++) {
+				lhm.remove(i);
+			}
+			endTime = System.nanoTime();
+			duration = endTime - startTime;
+			System.out.println("LinkedHashMap remove:  " + duration);
+		
+			// TreeMap
+			 TreeMap<Integer, String> tm = new TreeMap<Integer, String>();
+			 
+				
+				// PUT
+				startTime = System.nanoTime();
+				for (Integer i = 0; i < n; i++) {
+					tm.put(i, "John Smith");
+				}
+				endTime = System.nanoTime();
+				duration = endTime - startTime;
+				System.out.println("TreeMap puts:  " + duration);
+				
+				// GET
+				startTime = System.nanoTime();
+				for (int i = 0; i < n; i++) {
+					tm.get(i);
+				}
+				endTime = System.nanoTime();
+				duration = endTime - startTime;
+				System.out.println("TreeMap gets:  " + duration);
+				
+				// REMOVE
+				startTime = System.nanoTime();
+				for (int i = 0; i < n; i++) {
+					tm.remove(i);
+				}
+				endTime = System.nanoTime();
+				duration = endTime - startTime;
+				System.out.println("TreeMap remove:  " + duration);
+			 
 	}
 }
