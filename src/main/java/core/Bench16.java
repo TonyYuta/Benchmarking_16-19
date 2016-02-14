50d50ff08b35af000d9953ad8ffd227879b54af9
TonyYuta <tonykagaya@gmail.com>
2016-02-13 17:54:48 -0800

    Branch16

---
+++ src/main/java/core/Bench16.java
@@ -0,0 +1,28 @@
+/**
+ *   File Name: Bench16.java<br>
+ *
+ *   Yutaka<br>
+ *   Created: Feb 13, 2016
+ *   
+ */
+
+package core;
+
+/**
+ * Bench16 //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Yutaka
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class Bench16 {
+
+	public static void main(String[] args) {
+		System.out.println("Bench 16"); 
+	}
+}
