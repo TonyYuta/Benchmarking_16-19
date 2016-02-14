3cca3595b371ab01dbad916568fac4027f7f75c9
TonyYuta <tonykagaya@gmail.com>
2016-02-13 18:21:47 -0800

    performance comparison between byte vs. Byte by adding the value to itself 1,000,000,000 times

--- src/main/java/core/Bench16.java
+++ src/main/java/core/Bench16.java
@@ -23,6 +23,29 @@ package core;
 public class Bench16 {
 
 	public static void main(String[] args) {
-		System.out.println("Bench 16"); 
+		long num = 1_000_000_000;
+		
+		//performance comparison between byte vs. Byte by adding the value to itself 1,000,000,000 times 
+		byte b = 0;
+		final long startB = System.currentTimeMillis();
+		for (int i = 0; i < num; i++) {
+			++b;
+		}
+		final long finishB = System.currentTimeMillis();
+		final long resultB = (finishB - startB);
+		System.out.print("byte adding time, ms: ");
+		System.out.println(resultB);
+		
+		Byte wb = 0;
+		final long startWb = System.currentTimeMillis();
+		for (int i = 0; i < num; i++) {
+			++wb;
+		}
+		final long finishWb = System.currentTimeMillis();
+		final long resultWb = (finishWb - startWb);
+		System.out.print("Byte adding time, ms: ");
+		System.out.println(resultWb);
+		
+		System.out.println("byte adding is quickly then Byte adding in " + resultWb/resultB + " times");
 	}
 }
